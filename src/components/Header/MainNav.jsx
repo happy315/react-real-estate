f9773a8378feeea5010dea03f5cd217221729be7
happy315 <singhjosan2317@gmail.com>
2022-05-03 15:12:13 +0530

    Build:make dropdown dynamic i do this when user click on any ink in dropdown then dropdown hide and use go to another page First i create a hideDropDownHandler and then attach with dropdown links and set dropdown to false when user click on link in dropdown

--- src/components/Header/MainNav.jsx
+++ src/components/Header/MainNav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { IoMdArrowDropdown } from 'react-icons/io';
+import { IoMdArrowDropdown } from "react-icons/io";
 const Nav = styled.div`
   display: flex;
   justify-content: space-around;
@@ -18,67 +18,69 @@ const Dropdowns = styled.div`
   ul {
     list-style: none;
     display: flex;
-    
   }
   ul li {
     padding: 0rem 2rem;
     font-size: 1.2rem;
     cursor: pointer;
   }
-  ul li .link{
-  cursor: pointer;
-  text-decoration: none;
-  color: #000;
+  ul li .link {
+    cursor: pointer;
+    text-decoration: none;
+    color: #000;
   }
 `;
 const Blog = styled.div`
-   .link{
-    text-decoration:none;
+  .link {
+    text-decoration: none;
     letter-spacing: 0.2rem;
     font-weight: 320;
     font-size: 1.5rem;
-    color:#000;
+    color: #000;
   }
 `;
 const FirstDropDown = styled.ul`
-      width: 250px;
-      background-color: #000;
-      position: absolute;
-      top: 6.2rem;
-      padding: 1rem 0rem;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      animation: animation-one 1.5s alternate forwards;
-      
-      @keyframes animation-one {
-         from{
-           transform: translateY(0);
-           opacity: 0;
-         }
-         to{
-           transform: translateY(0);
-           opacity: 1;
-         }
-      }
-    li{
-        margin: 0.5rem 0;
-      }
-     li .Flink{
-       color: #fff;
-       text-decoration:none;
-       font-size: 1.2rem;
-     } 
+  width: 250px;
+  background-color: #000;
+  position: absolute;
+  top: 6.2rem;
+  padding: 1rem 0rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  animation: animation-one 1.5s alternate forwards;
+
+  @keyframes animation-one {
+    from {
+      transform: translateY(0);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+  li {
+    margin: 0.5rem 0;
+  }
+  li .Flink {
+    color: #fff;
+    text-decoration: none;
+    font-size: 1.2rem;
+  }
 `;
 
 const MainNav = () => {
-   
   const [dropdown, setDropDown] = useState(false);
 
   const firstDropDownHandler = () => {
-         setDropDown(!dropdown)
+    setDropDown(!dropdown);
   }
-  
+
+  // When someone click on link so dropdown hide and user go to another page
+  const hideDropDownHandler = () => {
+    setDropDown(false);
+  }
 
   return (
     <Nav>
@@ -86,27 +88,75 @@ const MainNav = () => {
       <Dropdowns>
         <ul>
           <li>
-            <Link className="link" to='/' onClick={firstDropDownHandler }>Our Guides <IoMdArrowDropdown /></Link>
-            {dropdown && <FirstDropDown>
-              <li><Link className="Flink" to='/Our-LifeStle-Guides'>Our Lifestyle Guides</Link></li>
-              <li><Link className="Flink" to='/Our-Buying-Guides'>Our Buying Guides</Link></li>
-              <li><Link className="Flink" to='/'>For Sellers</Link></li>
-              <li><Link className="Flink" to='/'>For Downsizing</Link></li>
-              <li><Link className="Flink" to='/'>For Condos</Link></li>
-              <li><Link className="Flink" to='/'>Get All Our Guides</Link></li>
-              <li><Link className="Flink" to='/'>The Collection Magazine by Compass</Link></li>
-            </FirstDropDown>}
-            
-            
+            <Link className="link" to="/" onClick={firstDropDownHandler}>
+              Our Guides <IoMdArrowDropdown />
+            </Link>
+            {dropdown && (
+              <FirstDropDown>
+                <li>
+                  <Link className="Flink" to="/Our-LifeStle-Guides" onClick={hideDropDownHandler}>
+                    Our Lifestyle Guides
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Flink" to="/Our-Buying-Guides" onClick={hideDropDownHandler}>
+                    Our Buying Guides
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Flink" to="/" onClick={hideDropDownHandler}>
+                    For Sellers
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Flink" to="/" onClick={hideDropDownHandler}>
+                    For Downsizing
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Flink" to="/" onClick={hideDropDownHandler}>
+                    For Condos
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Flink" to="/" onClick={hideDropDownHandler}>
+                    Get All Our Guides
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Flink" to="/" onClick={hideDropDownHandler}>
+                    The Collection Magazine by Compass
+                  </Link>
+                </li>
+              </FirstDropDown>
+            )}
           </li>
-          <li><Link className="link" to='/'>Selling</Link></li>
-          <li><Link className="link" to='/'>Buying</Link></li>
-          <li><Link className="link" to='/'>Listing</Link></li>
-          <li><Link className="link" to='/'>Our Team</Link></li>
+          <li>
+            <Link className="link" to="/">
+              Selling
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/">
+              Buying
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/">
+              Listing
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/">
+              Our Team
+            </Link>
+          </li>
         </ul>
       </Dropdowns>
       <Blog>
-        <Link className="link" to='/'>Blog</Link>
+        <Link className="link" to="/">
+          Blog
+        </Link>
       </Blog>
     </Nav>
   );
