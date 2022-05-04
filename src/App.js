9372e245825389133052553e564fe62d0d6fec03
happy315 <singhjosan2317@gmail.com>
2022-05-04 14:58:48 +0530

    Build:Add client side routing through React Router Dom Why:Because i Want to build single page application How:First i add Redirect and then i add Routes in switch statement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
-
+import {Switch,Route} from 'react-router-dom'
+import { Redirect } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Redirect to="/" />
+      
+      <Switch>
+        <Route>
+        <Header />
+        </Route>
+      </Switch>
     </div>
   );
 }
